2f883e569db1d9cf937ce25a2ad229a2a9e7452a
Yu <yu@matrix.works>
2022-11-30 23:56:09 +0800

    feat: add more demo test cases

--- test/demo.sh
+++ test/demo.sh
@@ -1,5 +1,9 @@
 curl -v -H "app-id:fimatrix" -H "app-token:fimatrix2020" -H "x-uid: 9" \
-  -H "proxy-backend:http://localhost:8080/" \
+  -H "proxy-backend:http://localhost:8090/" \
   -H "proxy-topic:fap_demo" \
+  -d '{"name": "yufangbin", "age": 20}' \
+http://localhost:4035/dp/fdtx/extract_file_tables
+
+curl -v -H "app-id:fimatrix" -H "app-token:fimatrix2020" -H "x-uid: 9" \
   -d '{"name": "yufangbin", "age": 20}' \
 http://localhost:4035/dp/fdtx/extract_file_tables
